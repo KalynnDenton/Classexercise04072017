8922d5bdfc2e8cf451c2390189183bd6eeaa0637
kalynndenton <dentonkalynn12@gmail.com>
2017-04-08 01:26:37 -0400

    added to program

--- Inclass04072017/Program.cs
+++ Inclass04072017/Program.cs
@@ -9,8 +9,12 @@ namespace Inclass04072017
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //data type, variable name, is new, datatype, constructors.
             Person person1 = new Person("Amy", 30, "Washington, DC", "Chicken");
+            Person person2 = new Person("Bob", 22, "Cleveland, OH", "Beef");
+
+            Console.WriteLine(person1.name);
+            Console.WriteLine(person2.name);
         }
     }
 }
