8a40004b1840b4b9205998d2c23b84322cb07b40
kalynndenton <dentonkalynn12@gmail.com>
2017-04-07 23:54:25 -0400

    changes made

--- Inclass04072017/Person.cs
+++ Inclass04072017/Person.cs
@@ -8,5 +8,24 @@ namespace Inclass04072017
 {
     class Person
     {
+        public string name;
+        public int age;
+        public string homeTown;
+        public string favFood;
+
+        public Person()//Default constructor
+        {
+
+        }
+
+        public Person(string name, int age, string homeTown, string favFood)//Constructor with parameters
+        {
+            this.name = name;
+            this.age = age;
+            this.homeTown = homeTown;
+            this.favFood = favFood;
+
+        }
     }
+    
 }
